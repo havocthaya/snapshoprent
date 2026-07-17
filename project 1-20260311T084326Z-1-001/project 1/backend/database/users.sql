e069051ae3f799b2b7150f3105571eb31c4b5f95
thayanithi <maheshthayanithi9944@gmail.com>
2026-07-17 12:22:45 +0530

    Delete project 1-20260311T084326Z-1-001/project 1 directory

--- project 1-20260311T084326Z-1-001/project 1/backend/database/users.sql
+++
@@ -1,5 +0,0 @@
--- SQL Dump for users
--- Generated at 11/3/2026, 2:52:23 pm
-
-INSERT INTO users (_id, name, email, password, isAdmin, createdAt, updatedAt) VALUES ('"69afe2712fc9c110df2d83f0"', 'Thayanithi ', 'maheshthayanithi9944@gmail.com', '$2b$10$S2CEpotfRExNaKUVCb6xKO3yK3BaOB.3fP4I3GD6WumENkFN670Ia', false, '"2026-03-10T09:20:49.918Z"', '"2026-03-10T09:20:49.918Z"');
-INSERT INTO users (_id, name, email, password, isAdmin, createdAt, updatedAt) VALUES ('"69afe346a9c4fd897134dc34"', 'Admin User', 'admin@minishop.com', '$2b$10$Y4jZMv6zcSRChY.EH2IgOu77Os0Lug0E26g/nnraN4y5uoZk/b4LC', true, '"2026-03-10T09:24:22.075Z"', '"2026-03-10T09:24:22.075Z"');
