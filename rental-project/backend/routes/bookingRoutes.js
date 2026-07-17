aa5f4676cc9c5982019b6b7a982791fa9748ced8
thayanithi <maheshthayanithi9944@gmail.com>
2026-07-17 12:22:56 +0530

    Delete rental-project directory

--- rental-project/backend/routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addBookingItems, getMyBookings } = require('../controllers/bookingController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, addBookingItems);
-router.get('/mybookings', protect, getMyBookings);
-
-module.exports = router;
--- rental-project/backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authUser, registerUser } = require('../controllers/userController');
-
-router.post('/', registerUser);
-router.post('/login', authUser);
-
-module.exports = router;
